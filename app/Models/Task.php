f047e5d00439dbd5f9c2739bed44b05a51403510
Joao Douglas <josantos.inv@gmail.com>
2026-02-28 11:02:22 -0300

    feat: Criação da model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
+    // Relacionamento
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
