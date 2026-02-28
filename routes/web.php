836b54d4a05e9302bc53e4227cf0f4ba44e06119
Joao Douglas <josantos.inv@gmail.com>
2026-02-28 12:42:56 -0300

    feat: Alteração de navegação inicial para login

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
